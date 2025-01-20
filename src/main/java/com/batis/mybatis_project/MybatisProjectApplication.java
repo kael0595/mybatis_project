e3e5606fddfba74a10325b175e27b92f21c82d25
kael0595 <changgyu549@gmail.com>
2025-01-20 18:07:38 +0900

    init + gitignore(DS_Store)

---
+++ src/main/java/com/batis/mybatis_project/MybatisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.batis.mybatis_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/batis/mybatis_project/MybatisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batis.mybatis_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
